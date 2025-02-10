b7c61d2ddd42a0bd02a19db39c635d90c7de3cf1
Renato Esteves <36163846+renatoesteves@users.noreply.github.com>
2025-02-09 21:13:43 -0300

    beta

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,12 @@ import{CreateProductController} from './controllers/product/CreateProductControl
 import { ListByCategoryController } from './controllers/product/ListByCategory';
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoverOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrdersController } from './controllers/order/ListOrdersController';
+import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 import uploadConfig from './config/multer'
 
 const router = Router();
@@ -38,5 +44,12 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // -- ROTAS Order --
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoverOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders', isAuthenticated, new ListOrdersController().handle)
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
+
 
 export{ router };
